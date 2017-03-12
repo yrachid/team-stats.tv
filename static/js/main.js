803207b2b21a13881fa9a63ff5c4e160c2aff035
othman853 <anuar.yasser@gmail.com>
2017-03-12 01:31:38 -0300

    Trying to fix sizing bug

--- static/js/main.js
+++ static/js/main.js
@@ -7,24 +7,28 @@ const body = document.getElementsByTagName('body')[0];
 
 const tiles = document.getElementById('tiles');
 
+const t = id => tilesViews.filter( tv => tv.getAttribute('id') === id)[0];
+const rl = id => t(id).reload();
+
 const tilesViews = data.tiles.map(tile => {
   const view = document.createElement('webview');
 
- Object.keys(tile).forEach(k => {
-   console.log(`${k} => ${tile[k]}`);
-   view.setAttribute(k, tile[k]);
- });
+  Object.keys(tile).forEach(k => {
+    view.setAttribute(k, tile[k]);
+  });
 
   view.setAttribute("autosize", "on");
   view.setAttribute("useragent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36");
   view.addEventListener('did-start-loading', (id => () => console.log(`${id} :: Loading`))(tile.id) );
-  view.addEventListener('did-stop-loading', (id => () => console.log(`${id} :: Loaded`))(tile.id) );
+  view.addEventListener('did-stop-loading', (id => () => {
+    console.log(`${id} :: Loaded`);
+    if ( id === 'g04' || id === 'g05') {
+      console.log('scroll');
+      t(id).executeJavaScript("document.querySelector('body').scrollTop = 358");
+    }
+  })(tile.id));
 
   return view;
 });
 
 tilesViews.forEach(tv => tiles.appendChild(tv));
-
-
-const t = id => tilesViews.filter( tv => tv.getAttribute('id') === id)[0];
-const rl = id => t(id).reload();
