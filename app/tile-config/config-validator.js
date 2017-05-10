925f38014508d767a4d93622b0ed046825878be4
othman853 <anuar.yasser@gmail.com>
2017-05-10 10:58:08 -0300

    Styled error page and messages

--- app/tile-config/config-validator.js
+++ app/tile-config/config-validator.js
@@ -1,15 +1,15 @@
 module.exports = configuration => new Promise((resolve, reject) => {
 
   if (!configuration.tiles) {
-    return reject(new Error('The tiles section in configuration file is mandatory'));
+    return reject(new Error('Config file has no \'tiles\' section'));
   }
 
   if (configuration.tiles.constructor.name !== 'Array') {
-    return reject(new Error('The tiles section in configuration file must be an Array'));
+    return reject(new Error('The \'tiles\' section of config file is not an Array. It should be. With items on it.'));
   }
 
   if (configuration.tiles.length === 0) {
-    return reject(new Error('The tiles array must have items'));
+    return reject(new Error('The \'tiles\' array must have items'));
   }
 
   return resolve(configuration);
