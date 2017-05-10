970618c54e2bec557f93bdbf7f5f5611b27edf34
othman853 <anuar.yasser@gmail.com>
2017-05-10 05:07:04 -0300

    Renaming components

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,6 +1,4 @@
 const optimize = require(__dirname + '/static/js/optimize.js');
-const tileCreator = require(__dirname + '/static/js/components/tile.js');
-const tileControlsCreator = require(__dirname + '/static/js/components/tile-control.js');
 const ipc = require('electron').ipcRenderer;
 
 const tiles = document.getElementById('tiles');
