fe732f65695a8dc692daa63295fb9e0301576250
othman853 <anuar.yasser@gmail.com>
2017-05-21 00:43:30 -0300

    Testing interactions of validator with validation libs

--- test/unit/main/config/tiles/validator.spec.js
+++ test/unit/main/config/tiles/validator.spec.js
@@ -1,19 +1,51 @@
 const validate = require('../../../../../app/main/config/tiles/validator');
 
-// TODO: Check interactions with AJV
-describe.skip('unit -> main -> config -> tiles -> validator', () => {
+describe('unit -> main -> config -> tiles -> validator', () => {
 
-  const validator = td.function();
+  const ajvValidator = td.function();
   const ajv = { compile: td.function() };
-  const schema = {};
+  const schema = { whatever: true };
 
-  // const validator = validate(ajv, schema);
+  const validator = validate(ajv, schema);
 
-  it('', done => {
+  it('Should resolve the received config', done => {
 
-    when(ajv.compile(schema)).thenReturn(validator);
-    when()
+    const config = { someConfig: true };
+    td.when(ajv.compile(schema)).thenReturn(ajvValidator);
+    td.when(ajvValidator(config)).thenReturn(true);
 
+    validator(config)
+      .then(validConfig => {
+        expect(validConfig).to.eql(config);
+        done();
+      })
+      .catch(done);
+
+  });
+
+  it('Should reject the received config', done => {
+
+    const config = { someConfig: true };
+    td.when(ajv.compile(schema)).thenReturn(ajvValidator);
+    td.when(ajvValidator(config)).thenReturn(false);
+
+    validator(config)
+      then( () => done(new Error('Validator should have rejected config')))
+      .catch(() => done());
+  });
+
+  it.skip('Should reject the received config and send an error with formatted message', done => {
+    const ajvValidatorWithErrors = function() { return false };
+    ajvValidatorWithErrors.errors = [{ dataPath: 'p', message: 'bla' }];
+    const config = { someConfig: true };
+    td.when(ajv.compile(schema)).thenReturn(ajvValidatorWithErrors);
+
+    validator(config)
+      .then( () => done(new Error('Validator should have rejected config')))
+      .catch(error => {
+        expect(error.message).to.contain('Failed to parse configuration:')
+        done();
+      });
   });
 
 });
