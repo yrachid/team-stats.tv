455cd5b4d84baea1f713aa400885512356d4ebce
othman853 <anuar.yasser@gmail.com>
2017-05-22 03:13:05 -0300

    WIP Adding Spectron

---
+++ test/functional/team-stats.spec.js
@@ -0,0 +1,28 @@
+const Application = require('spectron').Application;
+
+describe.skip('functional', function() {
+  this.timeout(10000);
+
+  beforeEach(function() {
+    this.app = new Application({
+      path: path.join(process.cwd(), 'node_modules', '.bin', 'electron'),
+      args: [path.resolve('app/main/index.js')]
+    });
+
+    return this.app.start();
+  });
+
+  afterEach(function() {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
+  });
+
+  it('Should show the initial screen', function(done) {
+    return this.app.client.getWindowCount().then(function (count) {
+      expect(count).to.equal(1);
+      done();
+    })
+  });
+
+})
