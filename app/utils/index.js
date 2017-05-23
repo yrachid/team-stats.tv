e85bf16435968e2e2abd11e43fc97f14cdb6cc57
othman853 <anuar.yasser@gmail.com>
2017-05-22 21:02:13 -0300

    Adding utils module

---
+++ app/utils/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  object: Object,
+  json: JSON
+};
---
+++ test/unit/utils.spec.js
@@ -0,0 +1,10 @@
+describe('unit -> utils', () => {
+
+  const utils = solve('app/utils');
+
+  it('should be a proxy to global objects', () => {
+    expect(utils.json).to.eql(JSON);
+    expect(utils.object).to.eql(Object);
+  });
+
+});
