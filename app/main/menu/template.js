074c4da926c21afdcb4eac5863372b447ae05340
othman853 <anuar.yasser@gmail.com>
2017-06-07 15:50:46 -0300

    #32 (WIP): Making menu item visible

--- app/main/menu/template.js
+++ app/main/menu/template.js
@@ -1,11 +1,13 @@
 const reload = require('./items/reload')
 const quit = require('./items/quit')
+const reconfig = require('./items/reconfig')
 
 module.exports = (window, app) => ([
   {
     label: 'Window',
     submenu: [
       reload(window, app),
+      reconfig(window, app),
       { type: 'separator' },
       quit(window, app)
     ]
--- test/unit/main/menu/template.spec.js
+++ test/unit/main/menu/template.spec.js
@@ -1,10 +1,12 @@
 describe('unit -> main -> menu -> template', () => {
 
   const reload = td.function()
+  const reconfig = td.function()
   const quit = td.function()
 
   const templateBuilder = solve('app/main/menu/template', {
     './items/reload': reload,
+    './items/reconfig': reconfig,
     './items/quit': quit
   })
 
@@ -16,11 +18,13 @@ describe('unit -> main -> menu -> template', () => {
     const app = { whatever: 'app' }
     const window = { whatever: 'window' }
     const reloadMenuItem = { item: 'reload' }
+    const reconfigMenuItem = { item: 'reconfig' }
     const quitMenuItem = { item: 'quit' }
     const separator = { type: 'separator' }
 
     td.when(reload(window, app)).thenReturn(reloadMenuItem)
     td.when(quit(window, app)).thenReturn(quitMenuItem)
+    td.when(reconfig(window, app)).thenReturn(reconfigMenuItem)
 
     const template = templateBuilder(window, app)
 
@@ -28,10 +32,11 @@ describe('unit -> main -> menu -> template', () => {
     expect(template.length).to.equal(1)
     expect(template[0].label).to.equal('Window')
 
-    expect(template[0].submenu.length).to.equal(3)
+    expect(template[0].submenu.length).to.equal(4)
     expect(template[0].submenu[0]).to.eql(reloadMenuItem)
-    expect(template[0].submenu[1]).to.eql(separator)
-    expect(template[0].submenu[2]).to.eql(quitMenuItem)
+    expect(template[0].submenu[1]).to.eql(reconfigMenuItem)
+    expect(template[0].submenu[2]).to.eql(separator)
+    expect(template[0].submenu[3]).to.eql(quitMenuItem)
   })
 
 })
